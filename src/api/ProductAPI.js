c422ba3be91ef2005ff13918ba0662394f06ce57
Vinit Patidar <Vinitpatidar780@gmail.com>
2024-06-26 13:08:22 +0530

    .

--- src/api/ProductAPI.js
+++ src/api/ProductAPI.js
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 
 export const fetchProducts = async (category, setProductsData, setProducts, setIsError) => {
     try {
-        const { data } = await axios.get(`https://ecommerce-flask-api.vercel.app/api/v1/${category}`);
+        const { data } = await axios.get(`https://ecommerceflaskapi.vercel.app/api/v1/${category}`);
         setProducts(data);
         setProductsData(data);
     } catch (error) {
